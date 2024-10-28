3f67dcb2a24ec7f5519dcc1afef7289ef9edbcd0
roy-sourav23 <rsourav2023@gmail.com>
2024-10-28 21:13:14 +0530

    added code for analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <Analytics />
+      <SpeedInsights />
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
